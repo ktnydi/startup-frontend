b1f44b31b7e2dcfd2ff086d7fc2add4acac31490
kitano <kitano.prog@gmail.com>
2020-03-18 23:07:52 +0900

    Add tabbar

--- src/src/component/Dashboard.js
+++ src/src/component/Dashboard.js
@@ -1,15 +1,35 @@
 import React from 'react';
-import { Avatar } from '@material-ui/core';
+import { Avatar, Tabs, Tab } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import theme from '../asset/Theme';
 
 export default function Dashboard() {
+  const [index, setIndex] = React.useState(0);
   const classes = useStyles();
+
+  const handleChangeTab = (e, newValue) => (
+    setIndex(newValue)
+  )
 
   return(
     <div className={classes.root}>
       <div className={classes.signinUser}>
         <Avatar alt='login user image' src='https://source.unsplash.com/random/80x80' className={classes.avatar} />
         <div className={classes.name}>ゲストユーザー</div>
+      </div>
+      <div className={classes.tabBar}>
+        <Tabs
+          indicatorColor='primary'
+          variant='fullWidth'
+          value={index}
+          onChange={(e, newValue) => handleChangeTab(e, newValue)}
+          TabIndicatorProps={{
+            className: classes.indicator,
+          }}
+        >
+          <Tab label='募集' className={classes.tab} disableFocusRipple={true} disableRipple={true} />
+          <Tab label='応募' className={classes.tab} disableFocusRipple={true} disableRipple={true} />
+        </Tabs>
       </div>
     </div>
   );
@@ -34,5 +54,17 @@ const useStyles = makeStyles({
   },
   name: {
     margin: '15px 0 0',
+  },
+  tabBar: {
+    width: '100%',
+    margin: '30px 0 0',
+    border: '1px solid #ddd',
+    borderRadius: 3,
+  },
+  tab: {
+    fontSize: '1.5rem',
+  },
+  indicator: {
+    backgroundColor: theme.palette.primary.main,
   },
 });
