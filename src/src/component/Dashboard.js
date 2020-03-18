833ba1953aaa6e79c798ef8fb708d6a1cb8e7252
kitano <kitano.prog@gmail.com>
2020-03-18 15:31:52 +0900

    Add signin user info

--- src/src/component/Dashboard.js
+++ src/src/component/Dashboard.js
@@ -1,7 +1,38 @@
 import React from 'react';
+import { Avatar } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
 export default function Dashboard() {
+  const classes = useStyles();
+
   return(
-    <div>Dashboard.js</div>
+    <div className={classes.root}>
+      <div className={classes.signinUser}>
+        <Avatar alt='login user image' src='https://source.unsplash.com/random/80x80' className={classes.avatar} />
+        <div className={classes.name}>ゲストユーザー</div>
+      </div>
+    </div>
   );
 }
+
+const useStyles = makeStyles({
+  root: {
+    width: '100%',
+    maxWidth: 600,
+    margin: '0 auto',
+    padding: '80px 15px 40px',
+    color: 'rgba(0, 0, 0, 0.75)',
+  },
+  signinUser: {
+    textAlign: 'center',
+  },
+  avatar: {
+    display: 'inline-block',
+    width: 120,
+    height: 120,
+    border: '1px solid #ddd',
+  },
+  name: {
+    margin: '15px 0 0',
+  },
+});
