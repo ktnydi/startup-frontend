1fefa3784da2bec993c7424b6ed94e98c42873e0
kitano <kitano.prog@gmail.com>
2020-03-23 18:15:09 +0900

    Add firebase firestore and functions

--- src/src/firebase.js
+++ src/src/firebase.js
@@ -1,6 +1,10 @@
 import firebase from 'firebase/app';
 import firebaseConfig from './firebaseConfig';
 import 'firebase/auth';
+import 'firebase/firestore';
+import 'firebase/functions';
 
 firebase.initializeApp(firebaseConfig);
 export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+export const functions = firebase.functions();
