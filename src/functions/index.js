82055b7fdec7f44d9e5bc84a4a29ed63057a4bd4
kitano <kitano.prog@gmail.com>
2020-03-24 19:50:24 +0900

    Implement create profile

--- src/functions/index.js
+++ src/functions/index.js
@@ -1,8 +1,13 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.createProfile = functions.auth.user().onCreate(user => {
+  const uid = user.uid;
+  let docRef = admin.firestore().collection('users').doc(uid)
+  docRef.set({
+    introduce: '',
+    skill: [],
+    location: '',
+  });
+})
