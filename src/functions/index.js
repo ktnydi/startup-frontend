2d5095e5d520a704c1eb9eb60e5c38c14010d8c5
Yudai Kitano <kitano.prog@gmail.com>
2020-04-08 14:22:58 +0900

    Add fields to profile

--- src/functions/index.js
+++ src/functions/index.js
@@ -4,8 +4,12 @@ admin.initializeApp();
 
 exports.createProfile = functions.auth.user().onCreate(user => {
   const uid = user.uid;
+  const displayName = user.displayName;
+  const photoURL = user.photoURL;
   let docRef = admin.firestore().collection('users').doc(uid)
   docRef.set({
+    displayName: displayName,
+    photoURL: photoURL,
     introduce: '',
     skill: [],
     location: '',
