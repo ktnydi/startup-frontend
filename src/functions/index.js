b5d2799fa464236aac7b9f1a61e28df997aed79d
Yudai Kitano <kitano.prog@gmail.com>
2020-04-09 00:14:22 +0900

    Fix null field of users

--- src/functions/index.js
+++ src/functions/index.js
@@ -2,10 +2,11 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-exports.createProfile = functions.auth.user().onCreate(user => {
+exports.createProfile = functions.auth.user().onCreate(async (user) => {
   const uid = user.uid;
-  const displayName = user.displayName;
-  const photoURL = user.photoURL;
+  const authedUser = await admin.auth().getUser(user.uid);
+  const displayName = authedUser.displayName;
+  const photoURL = authedUser.photoURL;
   let docRef = admin.firestore().collection('users').doc(uid)
   docRef.set({
     displayName: displayName,
